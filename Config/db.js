801d4c59cf3a0d358fef3e6316a9a932cafe608c
Sagar Pradhan <sagar.pradhan@kemsys.com>
2026-04-23 16:00:57 +0530

    fix

--- Config/db.js
+++ Config/db.js
@@ -1,10 +1,19 @@
 import mongoose from 'mongoose';
 import { ENV } from './env.js';
 
+mongoose.set('bufferCommands', false);
+
 const connectDB = async () => {
     try {
-        const conn = await mongoose.connect(ENV.MONGO_URI);
+        if (!ENV.MONGO_URI) {
+            throw new Error('MONGO_URI is missing');
+        }
+
+        const conn = await mongoose.connect(ENV.MONGO_URI, {
+            serverSelectionTimeoutMS: 10000,
+        });
         console.log(`MongoDB Connected: ${conn.connection.host}`);
+        console.log("mongoose.connection.readyState", mongoose.connection.readyState);
         return conn;
     } catch (error) {
         console.error(`Error: ${error.message}`);
